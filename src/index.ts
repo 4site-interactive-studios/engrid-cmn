6add2787b2ba80cee16e2c1242c05c7f1f157c55
bryancasler <bryan.casler@gmail.com>
2021-02-16 17:08:51 -0500

    Updated handling of background images to match upstream changes in engrid-scripts, and enabled other option modules.

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,16 @@
-import { Options, App } from "@4site/engrid-common";
+import { Options, App } from "@4site/engrid-common"; // Uses ENGrid via NPM
+// import { Options, App } from "../../engrid-scripts/packages/common"; // Uses ENGrid via Visual Studio Workspace
 import "./sass/main.scss";
 
 const options: Options = {
   ModalDebug: true,
   applePay: false,
-  CapitalizeFields: false,
+  CapitalizeFields: true,
   ClickToExpand: true,
+  CurrencySymbol: '$',
+  CurrencySeparator: '.',
+  ImageAttribution: true,
   onLoad: () => console.log("Starter Theme Loaded"),
-  onResize: () => console.log("Starter Theme Window Resized"),
-  submitLabel: "Give",
+  onResize: () => console.log("Starter Theme Window Resized")
 };
 new App(options);
