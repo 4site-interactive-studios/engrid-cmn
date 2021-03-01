e4fed147e80a5a3f55d767b83931a3d73217ea4d
bryancasler <bryan.casler@gmail.com>
2021-03-01 13:39:30 -0500

    Updates to Button handling upstream in ENGrid, plus sizing for radio's to buttons, and how hover state bottom border colors is handled

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
-import { Options, App } from "@4site/engrid-common"; // Uses ENGrid via NPM
-//import { Options, App } from "../../engrid-scripts/packages/common"; // Uses ENGrid via Visual Studio Workspace
+// import { Options, App } from "@4site/engrid-common"; // Uses ENGrid via NPM
+import { Options, App } from "../../engrid-scripts/packages/common"; // Uses ENGrid via Visual Studio Workspace
 import "./sass/main.scss";
 import "./scripts/main.js";
 
@@ -10,8 +10,9 @@ const options: Options = {
   ClickToExpand: true,
   CurrencySymbol: '$',
   CurrencySeparator: '.',
-  ImageAttribution: true,
+  MediaAttribution: true,
   SkipToMainContentLink: true,
+  SrcDefer: true,
   onLoad: () => console.log("Starter Theme Loaded"),
   onResize: () => console.log("Starter Theme Window Resized")
 };
