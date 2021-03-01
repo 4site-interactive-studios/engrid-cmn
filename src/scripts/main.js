62112ddac85c42a39fc4ab18ead65ead8db307e2
Thomas Stalcup Jr <tstalcupjr@gmail.com>
2021-03-01 08:08:28 -0500

    one time upsell

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,7 +3,7 @@ document.onreadystatechange = () => {
         let paypal = document.querySelectorAll('.en__field__item.paypal label');
         let ga = document.querySelectorAll('.section-header.gift-amount')[0];
         let recurrF = document.querySelectorAll('.en__field--recurrfreq')[0];
-
+        let inlineMonthly = document.querySelectorAll('.inline-monthly-upsell')[0];
 
         [].forEach.call(paypal, e=>{
             e.innerHTML = e.innerHTML + '<span></span>';
@@ -11,6 +11,10 @@ document.onreadystatechange = () => {
 
         if(ga && recurrF){
             recurrF.appendChild(ga.parentNode);
+        }
+
+        if (inlineMonthly && recurrF) {
+          ga.insertAdjacentElement("beforebegin", inlineMonthly);
         }
     }
 
