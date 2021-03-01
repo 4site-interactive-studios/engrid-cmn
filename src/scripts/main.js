aa9b37f18cbb04e7e4661f64f57cf4dce065d46a
Thomas Stalcup Jr <tstalcupjr@gmail.com>
2021-03-01 07:35:36 -0500

    moves gift amount

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,14 +2,15 @@ document.onreadystatechange = () => {
     if(document.readyState === "complete"){
         let paypal = document.querySelectorAll('.en__field__item.paypal label');
         let ga = document.querySelectorAll('.section-header.gift-amount')[0];
+        let recurrF = document.querySelectorAll('.en__field--recurrfreq')[0];
 
 
         [].forEach.call(paypal, e=>{
             e.innerHTML = e.innerHTML + '<span></span>';
         });
 
-        if(ga){
-            alert('yes');
+        if(ga && recurrF){
+            recurrF.appendChild(ga.parentNode);
         }
     }
 
