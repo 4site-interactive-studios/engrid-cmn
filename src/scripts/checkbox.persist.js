f151800adb0c763c5c22026c0a8be4cfd30af0b3
Fernando Santos <fern@ndo.io>
2021-08-24 17:22:02 -0300

    Checkbox Persist Component

---
+++ src/scripts/checkbox.persist.js
@@ -0,0 +1,31 @@
+/*
+  _  _  ____  _ _         ____  _             _ _
+ | || |/ ___|(_) |_ ___  / ___|| |_ _   _  __| (_) ___  ___
+ | || |\___ \| | __/ _ \ \___ \| __| | | |/ _` | |/ _ \/ __|
+ |__   _|__) | | ||  __/  ___) | |_| |_| | (_| | | (_) \__ \
+    |_||____/|_|\__\___| |____/ \__|\__,_|\__,_|_|\___/|___/
+
+ */
+
+Storage.prototype.setObj = function (key, obj) {
+  return this.setItem(key, JSON.stringify(obj));
+};
+Storage.prototype.getObj = function (key) {
+  return JSON.parse(this.getItem(key));
+};
+
+// If there's any checkbox on the page
+var checkboxes = document.querySelectorAll("input[type='checkbox']");
+
+console.log(checkboxes.length);
+
+if (checkboxes.length) {
+  for (var i = 0; i < checkboxes.length; i++) {
+    checkboxes[i].addEventListener("change", (event) => {
+      localStorage.setObj(event.target.name, event.target.checked);
+      console.log(event.target.name);
+      console.log(event.target.checked);
+    });
+    localStorage.setObj(checkboxes[i].name, checkboxes[i].checked);
+  }
+}
