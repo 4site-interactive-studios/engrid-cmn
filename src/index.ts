94ab34c254ce21abdc7fa2d189a4eff284fe6485
Fernando Santos <fern@ndo.io>
2021-08-24 16:56:07 -0300

    Page Data Replace Component

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,20 @@
 import { Options, App } from "@4site/engrid-common"; // Uses ENGrid via NPM
 // import { Options, App } from "../../engrid-scripts/packages/common"; // Uses ENGrid via Visual Studio Workspace
 import "./sass/main.scss";
-import "./scripts/main.js";
+import "./scripts/main";
+import "./scripts/pagedataReplace";
 
 const options: Options = {
   ModalDebug: true,
   applePay: false,
   CapitalizeFields: true,
   ClickToExpand: true,
-  CurrencySymbol: '$',
-  CurrencySeparator: '.',
+  CurrencySymbol: "$",
+  CurrencySeparator: ".",
   MediaAttribution: true,
   SkipToMainContentLink: true,
   SrcDefer: true,
   onLoad: () => console.log("Starter Theme Loaded"),
-  onResize: () => console.log("Starter Theme Window Resized")
+  onResize: () => console.log("Starter Theme Window Resized"),
 };
 new App(options);
